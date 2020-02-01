d07f4d901bc0e65ba44a3e275ef350dbb56e83d9
Arthur <arthuryoshinori@gmail.com>
2020-02-01 15:17:10 -0300

    - UI linda

---
+++ UnityProject/Assets/Scripts/UI/CommandWindow.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CommandWindow : MonoBehaviour
+{
+    public Button m_sendButton;
+    public TMP_InputField m_commandField;
+    public TextMeshProUGUI m_logText;
+
+    public ScrollRect m_logScrollRect;
+
+    public string m_invalidCommandMessage = "Invalid Command";
+
+    public delegate string onMessageSentDelegate(string sMessage);
+    public onMessageSentDelegate onMessageSentCallback;
+
+    private void Awake()
+    {
+        m_sendButton.onClick.RemoveAllListeners();
+        m_sendButton.onClick.AddListener(SendCommand);
+    }
+    
+    public void SendCommand()
+    {
+        string commandText = m_commandField.text;
+        string result = onMessageSentCallback?.Invoke(commandText);
+
+        m_commandField.text = string.Empty;
+    }
+
+    public void RegisterLog(string pMessage)
+    {
+        m_logText.text += pMessage;
+        m_logText.text += "\n";
+    }
+}
